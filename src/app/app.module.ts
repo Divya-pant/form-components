6db3e38a56fe53933a99f329cf33eb4c377d4c63
Divya-pant <109518248+Divya-pant@users.noreply.github.com>
2022-10-19 12:33:45 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TextFieldComponent } from './form-components/se-text-field/text-field/text-field.component';
+import { SelectComponent } from './form-components/se-select/select/select.component';
+import { RadioGroupComponent } from './form-components/se-radio-group/radio-group/radio-group.component';
+import { CheckboxComponent } from './form-components/se-checkbox/checkbox/checkbox.component';
+import { DatepickerComponent } from './form-components/se-datepicker/datepicker/datepicker.component';
+import { NumberFieldComponent } from './form-components/se-numberfield/number-field/number-field.component';
+import { SwitchComponent } from './form-components/se-switch/switch/switch.component';
+import { TextAreaComponent } from './form-components/se-textarea/text-area/text-area.component';
+import { DisplayOnlyComponent } from './form-components/se-display-only/display-only/display-only.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextFieldComponent,
+    SelectComponent,
+    RadioGroupComponent,
+    CheckboxComponent,
+    DatepickerComponent,
+    NumberFieldComponent,
+    SwitchComponent,
+    TextAreaComponent,
+    DisplayOnlyComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +34,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
